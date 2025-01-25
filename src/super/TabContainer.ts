3c2d36f17877fc85ce9df12ab35f59ffdad2419a
jhudson <jhudson>
2025-01-24 19:12:36 -0500

    Initial work.

---
+++ src/super/TabContainer.ts
@@ -0,0 +1,14 @@
+
+
+export class TabContainer{
+    parent:HTMLElement;
+    constructor(parent:HTMLElement){
+        this.parent=parent;
+    }
+    addTab(name: string){
+
+        let tabcontent = document.createElement("div");
+        return tabcontent;
+        
+    }
+}
